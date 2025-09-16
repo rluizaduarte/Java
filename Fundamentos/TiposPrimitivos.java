4628a75d8379657b913f5f47163bbcb1dd7556a2
rluizaduarte <riellyduarte75@gmail.com>
2025-09-16 18:06:09 -0300

    16/09

---
+++ Fundamentos/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+public class TiposPrimitivos {
+	public static void main(String[] args){
+		byte idade = 20;
+		short ano = 2025;
+		int cep = 53405190;
+		long contato = 985640276; 
+		float altura = 1.72f;
+		double salario = 10000.50;
+		boolean estudante = true;
+		char inicial = 'R';
+
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano atual: " + ano);
+		System.out.println("CEP: " + cep);
+		System.out.println("Número para contato " + contato);
+		System.out.println("Altura: " + altura);
+		System.out.println("Salario: " + salario);
+		System.out.println("É estudante? " + estudante);
+		System.out.println("Inicial do nome: " + inicial);
+	}
+}
---
+++ Fundamentos/Variaveis.java
@@ -0,0 +1,6 @@
+public class Variaveis {
+	public static void main(String[] args){
+		int farinha = 2;
+		System.out.println("A receita requer " + farinha + " xícaras de farinha.");
+	}
+}
