9f801a320603506ae85baa335f302e5ee1ff49bf
rluizaduarte <riellyduarte75@gmail.com>
2025-12-06 20:11:01 -0300

    6/12

---
+++ Operadores/Aritmeticos.java
@@ -0,0 +1,13 @@
+public class Aritmeticos{
+	public static void main(String[] args) {
+		double a = 250;
+		double b = 10.5;
+		System.out.println("valor de a = " + a);
+		System.out.println("valor de b = " + b);
+		System.out.println("soma = " + (a + b));
+		System.out.println("diferença = " + (a - b));
+		System.out.println("produto = " + (a * b));
+		System.out.println("divisão = " + (a / b));
+		System.out.println("módulo (resto da divisão) = " + (a % b));
+	}
+}
---
+++ Operadores/DeComparacao.java
@@ -0,0 +1,14 @@
+public class DeComparacao{
+	public static void main(String[] args) {
+		float a = 10.5f;
+		float b = -20;
+		System.out.println("valor de a = " + a);
+		System.out.println("valor de b = " + b);
+		System.out.println("a é igual a b? " + (a == b));
+		System.out.println("a é diferente de b? " + (a != b));
+		System.out.println("a é maior que b? " + (a > b));
+		System.out.println("a é menor que b? " + (a < b));
+		System.out.println("a é maior ou igual a b? " + (a >= b));
+		System.out.println("a é menor ou igual a b? " + (a <= b));
+	}
+}
